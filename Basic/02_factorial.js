7849e9d0dfd7877cfb142333a04df4af908e5faf
Stefanos Kami <s.kaminakhs@gmail.com>
2017-05-13 20:48:46 +0300

    Add files via upload

---
+++ Basic/02_factorial.js
@@ -0,0 +1,37 @@
+// YouTube Video: https://youtu.be/43XF7GBY05E
+
+// Using iteration and functional programming
+function factorial_i(num) {
+
+    if (num === 0) {
+        return 1;
+    } else {
+        // create a range of numbers 1 through num
+        var array = [];
+        for (var i = 1; i <= num; i++) {
+            array.push(i);
+        }
+
+        // reduce the array of integers to one value
+        return array.reduce(function (a, b) {
+            return a * b;
+        });
+    }
+}
+
+// Using recursion
+function factorial_r(num) {
+
+    // This is the base case.
+    // Without it, you will have an infinite loop!
+    if (num <= 1) {
+        return 1;
+    } else {
+        return num * factorial_r(num - 1);
+    }
+}
+
+console.log("Using iteration: " + factorial_i(5));
+console.log("Using recursion: " + factorial_r(7));
+console.log("Using iteration: " + factorial_i(0));
+console.log("Using recursion: " + factorial_r(0));
---
+++ Basic/03_palindrome.js
@@ -0,0 +1,39 @@
+// YouTube Video: https://youtu.be/mQ6ZB24DInc
+
+// uses recursion
+function palindrome(str) {
+    if (str.length <= 1) {
+        return true;
+    } else if (str[0] === str.slice(-1)) {
+        return palindrome(str.slice(1, -1));
+    } else {
+        return false;
+    }
+}
+
+console.log(palindrome("radar"));
+console.log(palindrome("hello"));
+
+// uses RegEx (regular expressions)
+function palindromeRegex(str) {
+    str = str.toLowerCase().replace(/[\W_]/g, "");
+    if (str === str.split("").reverse().join("")) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// shorter way to write palindromeRegex()
+function palindromeRegex2(str) {
+    str = str.toLowerCase().replace(/[\W_]/g, "");
+    return str === str.split("").reverse().join("");
+
+    // a double or tripple equals will return a boolean (true/false)
+    // the first version works, but is repetitive.
+    // thanks to @revisualize for pointing that out 
+}
+
+
+console.log(palindromeRegex("A man. A plan. A canal. Panama."));
+console.log(palindromeRegex("Hello world!"));
